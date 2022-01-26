1ad3e05b55b3a404d136dcbb202ed1e2ab7c66d1
AlexFariaa <alexcaras1@hotmail.com>
2022-01-26 00:57:32 -0300

    projeto 2 finalizado (basic)

---
+++ Projeto2/Jokenpo.js
@@ -0,0 +1,86 @@
+console.clear()
+var pc = require('prompt-sync')();
+//Pedra ganha da tesoura, mas perde para o papel;
+//Tesoura ganha do papel, mas perde para a pedra;
+//Papel ganha da pedra, mas perde para a tesoura.
+
+var maquina = ['pedra', 'papel', 'tesoura']
+
+do {
+    var vit = 0
+    var der = 0
+    var empate = 0
+    var resp = ''
+    cont = 1
+    var rodadas = +pc("Quantas rodadas deseja jogar? ");
+    console.clear()
+while (cont <= rodadas)
+{
+    cont++
+    console.log("O que você escolhe entre: pedra, papel ou tesoura?") 
+    var sua_escolha = pc(`Resposta: `)
+    console.log()
+    var escolha_maquina = maquina[Math.floor(Math.random() * maquina.length)];
+    console.log(escolha_maquina);
+    if (sua_escolha == 'pedra' && escolha_maquina == 'tesoura' || sua_escolha == 'tesoura' && escolha_maquina == 'papel' || sua_escolha == 'papel' && escolha_maquina == 'pedra')
+    {
+        vit++
+        console.log();
+        console.log("=-=-=-=-=-=-=-=-=-")
+        console.log("Você ganhou!");
+        console.log("=-=-=-=-=-=-=-=-=-")
+        console.log()
+    } 
+    if (escolha_maquina == 'pedra' && sua_escolha == 'tesoura' || escolha_maquina == 'tesoura' && sua_escolha == 'papel' || escolha_maquina == 'papel' && sua_escolha == 'pedra')
+    {
+        der++
+        console.log();
+        console.log("=-=-=-=-=-=-=-=-=-")
+        console.log("Você perdeu!");
+        console.log("=-=-=-=-=-=-=-=-=-")
+        console.log()
+    }
+    if (escolha_maquina == 'pedra' && sua_escolha == 'pedra') 
+    {
+        empate++
+        console.log();
+        console.log("Empate");
+    }
+    if (escolha_maquina == 'tesoura' && sua_escolha == 'tesoura')
+    {
+        empate++
+        console.log();
+        console.log("Empate");
+    }
+    if (escolha_maquina == 'papel' && sua_escolha ==  'papel')
+    {
+        empate++
+        console.log();
+        console.log("Empate");
+    }
+}
+
+r1 = pc("Aperte ENTER para ver os resultados...")
+console.clear()
+if (vit > der)
+{
+console.log(`Você ganhou da máquina com incriveis: ${vit} Vitórias em cima dela!`);
+console.log(`UMA BESTA ENJAULADA!!`)
+console.log(`Curiosamente, nessa partida você teve: ${empate}`);
+}
+if (vit < der)
+{
+console.log(`Você foi derrotado pela maquina com: ${der} Vitórias para ela!`);
+console.log(`Talvez devessemos repensar sobre a evolução da tecnologia...`)
+console.log(`Curiosamente, nessa partida você teve: ${empate}`);
+} else if (vit == der){
+console.log(`Empates: ${vit} vitorias e ${der}`);
+}
+console.log()
+resp = pc("Deseja jogar novamente? ")
+console.clear()
+} while (resp !== 'nao')
+
+
+
+
